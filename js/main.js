a9778bc7f3d1560693c77497f4cc3f495ae7213d
fa-kohansal <farnaz_kohansal@yahoo.com>
2025-06-12 12:11:22 -0700

    add reset button

--- js/main.js
+++ js/main.js
@@ -25,3 +25,14 @@ document.addEventListener("DOMContentLoaded", function () {
     calcAge(event);
   });
 });
+ 
+
+let resetHandler=()=>{
+    document.getElementById("infoCard ").classList.remove("d-none");
+    document.getElementById("infoCard ").classList.add("d-flex");
+    document.getElementById("showAge").classList.remove("d-block");
+    document.getElementById("showAge").classList.add("d-none");
+    document.getElementById("username").innerHTML="";
+    document.getElementById("userAge").innerHTML="";
+    document.getElementById("clientName").value="";
+}
